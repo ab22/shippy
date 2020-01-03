b2dce6e73b537015f71203ef5d70c65ea38aa7d6
Abelardo Mendoza <abelardo22.9@gmail.com>
2020-01-02 22:15:35 -0600

    created vessel service

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -6,6 +6,7 @@ import (
 
 	// Import protobuf code
 	pb "github.com/ab22/shippy/consignment-service/proto/consignment"
+	vesselProto "github.com/ab22/shippy/vessel-service/proto/vessel"
 	"github.com/micro/go-micro"
 )
 
@@ -37,24 +38,35 @@ func (repo *Repository) GetAll() []*pb.Consignment {
 // in the generated code itself for the exact method signatures
 // to give you a better idea.
 type service struct {
-	repo repository
+	repo         repository
+	vesselClient vesselProto.VesselServiceClient
 }
 
 // CreateConsignment - we created just one method on our service,
 // which is a create method, which takes a context and a request as an
 // argument, these are handled by the gRPC server.
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
-	// Save our consignment
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity:  int32(len(req.Containers)),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	log.Println("Found vessel", vesselResponse.Vessel.Name)
+
 	consignment, err := s.repo.Create(req)
 
 	if err != nil {
 		return err
 	}
 
-	// Return matching the 'Response' message we created in our
-	// protobuf definition.
 	res.Created = true
 	res.Consignment = consignment
+	req.VesselId = vesselResponse.Vessel.Id
+
 	return nil
 }
 
@@ -65,21 +77,17 @@ func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *
 }
 
 func main() {
-	log.Println("Starting server...")
-	repo := &Repository{}
-
-	// Create a new service. Optionally include some options here.
-	srv := micro.NewService(
-		// This name must match the package name given in your protobuf definition
-		micro.Name("shippy.consignment.service"),
+	var (
+		srv          = micro.NewService(micro.Name("shippy.consignment.service"))
+		repo         = &Repository{}
+		vesselClient = vesselProto.NewVesselServiceClient("shippy.vessel.service", srv.Client())
 	)
 
+	log.Println("Starting server...")
 	srv.Init()
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
 
-	// Register handler
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
-
-	// Run server.
+	log.Println("Running service...")
 	if err := srv.Run(); err != nil {
 		log.Fatalf("Could not run server: %v", err)
 	}
---
+++ vessel-service/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	pb "github.com/ab22/shippy/vessel-service/proto/vessel"
+	micro "github.com/micro/go-micro"
+)
+
+type repository interface {
+	FindAvailable(*pb.Specification) (*pb.Vessel, error)
+}
+
+// VesselRepository ...
+type VesselRepository struct {
+	vessels []*pb.Vessel
+}
+
+// FindAvailable vessels.
+func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
+	for _, vessel := range repo.vessels {
+		if spec.Capacity <= vessel.Capacity && spec.MaxWeight <= vessel.MaxWeight {
+			return vessel, nil
+		}
+	}
+
+	return nil, errors.New("No vessel found by that spec")
+}
+
+type service struct {
+	repo repository
+}
+
+func (s *service) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error {
+	vessel, err := s.repo.FindAvailable(req)
+
+	if err != nil {
+		return err
+	}
+
+	res.Vessel = vessel
+	return nil
+}
+
+func main() {
+	var (
+		vessels = []*pb.Vessel{
+			&pb.Vessel{Id: "vessel001", Name: "Boaty McBoatface", MaxWeight: 200000, Capacity: 500},
+		}
+		repo = &VesselRepository{vessels}
+		srv  = micro.NewService(micro.Name("shippy.vessel.service"))
+	)
+
+	log.Println("Starting Vessel service...")
+	srv.Init()
+	pb.RegisterVesselServiceHandler(srv.Server(), &service{repo})
+
+	log.Println("Running server...")
+	if err := srv.Run(); err != nil {
+		log.Println("Error running server:", err)
+	}
+}
